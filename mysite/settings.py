1affc788a850e35c09dc0cf5daaa2926aeba0bf5
U. Jung <jung.uwe@gmail.com>
2020-09-04 09:57:47 +0200

    heroku integration in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
